6be8ba85eb452a4d4f46358b2fac1a1e41e89881
Carlos <89907733+biocompt@users.noreply.github.com>
2023-01-09 16:23:35 +0100

    Update SECOND_QC_post_imputation.sh

--- QC/SECOND_QC_post_imputation.sh
+++ QC/SECOND_QC_post_imputation.sh
@@ -6,41 +6,62 @@ CONCATENATED="path/to/concatenated"
 SCRIPTS="path/to/scripts_folder"
 
 ## 1) Extract each sample from the zip file downloaded
-cd $RAW
-for i in {1..22}
+## We will create separately scripts for each chromosome, so we can run all of them in one round
+ls $RAW > $SCRIPTS/raw_chr.txt
+mkdir $SCRIPTS/UNZIP
+for line in $SCRIPTS/raw_chr.txt
 do
-unzip -P "password" chr_${i}.zip -d $UNFILTERED/ &
+echo "!/bin/bash" > $SCRIPTS/UNZIP/${line::-4}.sh
+echo "unzip -P 'PASSWORD' $line -d ${UNFILTERED}" >> $SCRIPTS/UNZIP/${line::-4}.sh
 done
+
+for file in $SCRIPTS/UNZIP/*.sh
+do
+sbatch $file
+done
+
+echo "You have unzipped all the files"
 
 ## 2) Change name of the file to describe it
 cd $UNFILTERED
+# Move info files to a different folder
+mkdir ../info
+mv *.info ../info
+
 for file in *.vcf.gz
 do
 mv $file $(basename $file .dose.vcf.gz)_[DESCRIPTION_OF_THE_FILE].dose.vcf.gz
 done
 
-# Move info files to a different folder
-mkdir ../info
-mv *.info ../info
-
 ## 3) Indexing tab-delimited genome position
-for file in *.vcf.gz
+ls $UNFILTERED > $SCRIPTS/unzipped_chr.txt
+mkdir $SCRIPTS/TABIX
+for line in $SCRIPTS/unzipped_chr.txt
 do
-tabix -p vcf $file &
+echo "!/bin/bash" > $SCRIPTS/TABIX/${line::-7}.sh
+echo "tabix -p vcf $file" >> $SCRIPTS/TABIX/${line::-7}.sh
+done
+
+for file in $SCRIPTS/TABIX/*.sh
+do
+sbatch $file
 done
 
 ## 4) Filter by INFO imputation score
-for file in *.vcf.gz
+mkdir $SCRIPTS/FILTERED 
+for line in $SCRIPTS/unzipped_chr.txt
 do
-bcftools view -i 'MIN(INFO/R2)>0.3' chr${chr}_[DESCRIPTION_OF_THE_FILE].dose.vcf.gz --threads 8 | bgzip -c > $FILTERED/chr${chr}_[DESCRIPTION_OF_THE_FILE].dose.INFO0.3.vcf.gz
+echo "!/bin/bash" > $SCRIPTS/TABIX/${line::-7}.sh
+echo "bcftools view -i 'MIN(INFO/R2)>0.3' $line --threads 8 | bgzip -c > $FILTERED/${line::-7}.INFO_0.3.vcf.gz" >> $SCRIPTS/FILTERED/${line::-7}.sh
+done
+
+for file in $SCRIPTS/FILTERED/*.sh
+do
+sbatch $file
 done
 
 ## 5) Indexing filtered chromosomes
-cd $FILTERED
-for file in *.vcf.gz
-do
-tabix -p vcf $file &
-done
+## Repeat step 3
 
 ## 6) Concatenated all chromosomes in one file
 bcftools concat chr*.vcf.gz -o $CONCATENATED/OUTPUT
